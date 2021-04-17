2a4534d91144f9e343f00f70ee0f22e2fd4a771c
Hector Villarino <62912151+hector997@users.noreply.github.com>
2021-04-17 15:00:56 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+ <div id = "App">
+   <Header />
+   <AddTodo v-on:add-todo="addTodo" />
+   <ToDos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+ </div>
+</template>
+
+<script>
+import ToDos from './components/ToDos';
+import Header from './components/layout/header.vue';
+import AddTodo from './components/addTodo';
+
+export default {
+  name: 'App',
+  components: {
+      ToDos,
+      Header,
+      AddTodo
+  },
+  data() {
+    return {
+        todos: [
+          {
+            id:1,
+            title: "ToDo One",
+            completed: false
+          },
+          {
+            id:2,
+            title: "ToDo Two",
+            completed: true
+          },
+          {
+            id:3,
+            title: "ToDo three",
+            completed: false
+          },
+        ]
+      }
+    },
+    methods: {
+        deleteTodo(id) {
+          this.todos = this.todos.filter(todo => todo.id !== id);
+        },
+        addTodo(newTodo){
+          console.log(this.title);
+          this.todos = [...this.todos, newTodo];
+        }
+      }
+}
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+   }
+
+   body{
+     font-family: Arial, Helvetica, sans-serif;
+     line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+  
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-completed': todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo',todo.id)" class="del">x</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ToDoItem",
+    props: ["todo"],
+    data() {
+        return {
+            todoLocal: this.todo,
+        };
+    },
+    methods: {
+        markComplete() {
+            console.log("todo completed state changed");
+            this.todoLocal.completed = !this.todoLocal.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item{
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+    .is-completed {
+        text-decoration: line-through;
+    }
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+    
+</style>
